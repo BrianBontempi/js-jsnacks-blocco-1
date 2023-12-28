a7407ca2db98ed3a3df9749d6ed47baad95e53b5
BrianBontempi <brian.bont54@gmail.com>
2023-12-28 22:35:42 +0100

    complete 1 snack

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -9,3 +9,25 @@ console.log(fridge)
 
 fridge.push("pesca");
 console.log(fridge)
+
+// do un target al cocomero
+
+const targetElement = "cocomero";
+
+let elementoTrovato = false;
+
+// cerco il cocomero in frigo
+
+for (let i = 0; i < fridge.length; i++) {
+    if (fridge[i] === targetElement) {
+        elementoTrovato = true;
+        break;
+    }
+}
+
+// Verifica se l'elemento è stato trovato e stampa il messaggio appropriato
+if (elementoTrovato) {
+    console.log("Trovato! Devo solo preparare il cocktail.");
+} else {
+    console.log("Oh no, devo uscire a comprare il cocomero!");
+}
