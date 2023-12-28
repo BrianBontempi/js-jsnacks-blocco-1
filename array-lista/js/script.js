dc4c6c8554cbeb32fb90f88a3c5d1555ab27ad36
BrianBontempi <brian.bont54@gmail.com>
2023-12-28 22:25:22 +0100

    add array fridge and insert pesca

---
+++ array-lista/js/script.js
@@ -0,0 +1,11 @@
+console.log("JS OK")
+
+// creo l'array del frigo
+
+let fridge = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+console.log(fridge)
+
+// inserisco la pesca nel frigo
+
+fridge.push("pesca");
+console.log(fridge)
